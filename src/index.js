c3183b3ae7797536b69d3719c754150fcf3dadef
techmeg <42649602+techmeg@users.noreply.github.com>
2021-09-07 18:09:50 -0400

    change router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import App from './App'
 import {ContextProvider} from './Context'
 
 ReactDOM.render(
   <ContextProvider>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router >
       <React.StrictMode>
         <App />
       </React.StrictMode>
